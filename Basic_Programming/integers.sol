2204f8f89d3bb59d21c4ad97b69a05ac79f69e43
Chaitya Vora <90527884+vorachaitya@users.noreply.github.com>
2023-01-24 10:53:43 +0530

    integers

---
+++ Basic_Programming/integers.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+//int: can accept both positive and negative numbers  int8 to int256
+// int alias to int256
+
+//uint: can only accept positive numbers              uint8 to uint256
+// uint alias to uint256
+
+// by default,int is initialsed to 0
